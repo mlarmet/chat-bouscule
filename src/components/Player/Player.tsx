9746fb7d4b2df2895aa8eddcd971518e1c002779
Maxence LARMET <maxencelarmet@orange.fr>
2025-03-12 03:08:50 +0100

    feat: add audio when select character

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,5 +1,10 @@
 import { useGameStore } from "services/store";
 
+import useSound from "use-sound";
+
+import matouSound from "assets/audio/cat_cut.mp3";
+import minouSound from "assets/audio/kitten_cut.mp3";
+
 import "./Player.scss";
 
 interface PlayerProps {
@@ -9,7 +14,18 @@ interface PlayerProps {
 
 export default function Player({ player, visible }: PlayerProps) {
 	const players = useGameStore((state) => state.players);
+
 	const setSelectedPion = useGameStore((state) => state.setSelectedPion);
+
+	const [playMatou] = useSound([matouSound, minouSound], {
+		interrupt: true,
+		volume: 0.5,
+	});
+
+	const [playMinou] = useSound(minouSound, {
+		interrupt: true,
+		volume: 0.5,
+	});
 
 	const selectPion = (pion: PionType) => {
 		if (players[player].pions[pion].stock === 0) {
@@ -17,30 +33,34 @@ export default function Player({ player, visible }: PlayerProps) {
 		}
 
 		setSelectedPion(player, pion);
+
+		if (pion === "minou") {
+			playMinou();
+		} else if (pion === "matou") {
+			playMatou();
+		}
 	};
 
-	const getClass = (pion: PionType) => {
-		let classList = "";
-
+	const getState = (pion: PionType) => {
 		if (players[player].pions[pion].stock <= 0) {
 			return " disabled";
 		}
 
 		if (players[player].selectedPion === pion) {
-			classList += " selected";
+			return " selected";
 		}
 
-		return classList;
+		return "";
 	};
 
 	return (
 		<div id={player} className={"player" + (visible ? "" : " hide")}>
-			<div className={"sprite-selector" + getClass("minou")} onClick={() => selectPion("minou")}>
+			<div className={"sprite-selector" + getState("minou")} onClick={() => selectPion("minou")}>
 				<img className="sprite" src={`assets/sprites/minou_${player}.png`} alt="" />
 
 				<p>{players[player].pions.minou.stock}</p>
 			</div>
-			<div className={"sprite-selector" + getClass("matou")} onClick={() => selectPion("matou")}>
+			<div className={"sprite-selector" + getState("matou")} onClick={() => selectPion("matou")}>
 				<img className="sprite" src={`assets/sprites/matou_${player}.png`} alt="" />
 				<p>{players[player].pions.matou.stock}</p>
 			</div>
